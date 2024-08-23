4bf541ad28fd241b15b6566befd813518d29f85e
Tjoslef <josef.pasek17@gmail.com>
2024-08-23 19:56:06 +0200

    some day i hope i will finished it

--- Os_kubrt/src/lib.rs
+++ Os_kubrt/src/lib.rs
@@ -3,6 +3,10 @@
 #![feature(panic_info_message,asm)]
 
 use core::arch::asm;
+use core::usize;
+
+use self::page::{Table, PAGE_SIZE};
+use self::uart::Uart;
 mod uart;
 mod page;
 // ///////////////////////////////////
@@ -64,6 +68,52 @@ fn abort() -> ! {
 		}
 	}
 }
+unsafe extern "C" {
+    static TEXT_START: usize;
+	static TEXT_END: usize;
+	static DATA_START: usize;
+	static DATA_END: usize;
+	static RODATA_START: usize;
+	static RODATA_END: usize;
+	static BSS_START: usize;
+	static BSS_END: usize;
+	static KERNEL_STACK_START: usize;
+	static KERNEL_STACK_END: usize;
+	static HEAP_START: usize;
+	static HEAP_SIZE: usize;
+	static mut KERNEL_TABLE: usize;
+}
+pub fn id_map_range(root: &mut page::Table,
+start:usize,
+end: usize,
+bits: i64,
+
+){
+    let mud memaddr = start & !(page:PAGE_SIZE -1);
+    let num_kb_pages = (page::align_val(end,12)- memaddr)
+		/ page::PAGE_SIZE;
+    for _ in 0..num_kb_pages{
+    page::map(root, memaddr, memaddr, bits,0);
+    memaddr += 1 <<12;
+    }
+
+}
+unsafe extern "C" fn kinit() -> usize{
+ uart:Uart::new(0x1000_0000).init();
+    page:init();
+    kmen.init();
+
+    let root_ptr = kmen::get_page_table();
+    let root_u = root_ptr as usize;
+    let mut :
+
+
+
+
+
+
+}
+
 fn kmain(){
 let mut my_uart = uart::Uart::new(0x1000_0000);
 	my_uart.init();
