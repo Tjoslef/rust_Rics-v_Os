e0a8a5dbaa536d244cdf4c4f97b227e0684e1651
Tjoslef <josef.pasek17@gmail.com>
2024-08-23 22:10:53 +0200

    update

--- Os_kubrt/src/lib.rs
+++ Os_kubrt/src/lib.rs
@@ -61,9 +61,6 @@ extern "C"
 fn abort() -> ! {
 	loop {
 		unsafe {
-            // The asm! syntax has changed in Rust.
-            // For the old, you can use llvm_asm!, but the
-            // new syntax kicks ass--when we actually get to use it.
 			asm!("wfi");
 		}
 	}
@@ -84,6 +81,44 @@ unsafe extern "C" {
 	static mut KERNEL_TABLE: usize;
 }
 pub fn id_map_range(root: &mut page::Table,
+                    start: usize,
+                    end: usize,
+                    bits: i64)
+{
+	let mut memaddr = start & !(page::PAGE_SIZE - 1);
+	let num_kb_pages = (page::align_val(end, 12)
+	                 - memaddr)
+	                / page::PAGE_SIZE;
+
+	for _ in 0..num_kb_pages {
+		page::map(root, memaddr, memaddr, bits, 0);
+		memaddr += 1 << 12;
+	}
+}
+// ///////////////////////////////////
+// / ENTRY POINT
+// ///////////////////////////////////
+#[no_mangle]
+extern "C" fn kinit() -> usize {
+	// We created kinit, which runs in super-duper mode
+	// 3 called "machine mode".
+	// The job of kinit() is to get us into supervisor mode
+	// as soon as possible.
+	// Interrupts are disabled for the duration of kinit()
+	uart::Uart::new(0x1000_0000).init();
+	page::init();
+	kmem::init();
+
+	// Map heap allocations
+	let root_ptr = kmem::get_page_table();
+	let root_u = root_ptr as usize;
+	let mut root = unsafe { root_ptr.as_mut().unwrap() };
+	let kheap_head = kmem::get_head() as usize;
+	let total_pages = kmem::get_num_allocations();
+	println!();
+	println!();
+}
+pub fn id_map_range(root: &mut page::Table,
 start:usize,
 end: usize,
 bits: i64,
@@ -99,18 +134,119 @@ bits: i64,
 
 }
 unsafe extern "C" fn kinit() -> usize{
- uart:Uart::new(0x1000_0000).init();
-    page:init();
-    kmen.init();
+ uart::Uart::new(0x1000_0000).init();
+    page::init();
+    kmen::init();
 
     let root_ptr = kmen::get_page_table();
     let root_u = root_ptr as usize;
-    let mut :
+    let mut root = unsafe{root_ptr.as_mut().unwrap()};
+    let kheap_heap = kmen::get_head() as usize;
+    let total_pages = kmen::get_num_allocation();
+    println!();
+	println!();
+	unsafe {
+		println!("TEXT:   0x{:x} -> 0x{:x}", TEXT_START, TEXT_END);
+		println!("RODATA: 0x{:x} -> 0x{:x}", RODATA_START, RODATA_END);
+		println!("DATA:   0x{:x} -> 0x{:x}", DATA_START, DATA_END);
+		println!("BSS:    0x{:x} -> 0x{:x}", BSS_START, BSS_END);
+		println!("STACK:  0x{:x} -> 0x{:x}", KERNEL_STACK_START, KERNEL_STACK_END);
+		println!("HEAP:   0x{:x} -> 0x{:x}", kheap_head, kheap_head + total_pages * 4096);
+	}
+    id_map_range(&mut root, kheap_heap, kheap_heap +total_pages * 4096, page::EntryBits::ReadWrite.val());
+     unsafe {
+		// Map heap descriptors
+		let num_pages = HEAP_SIZE / page::PAGE_SIZE;
+		id_map_range(&mut root,
+					 HEAP_START,
+					 HEAP_START + num_pages,
+					 page::EntryBits::ReadWrite.val()
+		);
+		// Map executable section
+		id_map_range(
+		             &mut root,
+		             TEXT_START,
+		             TEXT_END,
+		             page::EntryBits::ReadExecute.val(),
+		);
+		// Map rodata section
+		// We put the ROdata section into the text section, so they can
+		// potentially overlap however, we only care that it's read
+		// only.
+		id_map_range(
+		             &mut root,
+		             RODATA_START,
+		             RODATA_END,
+		             page::EntryBits::ReadExecute.val(),
+		);
+		// Map data section
+		id_map_range(
+		             &mut root,
+		             DATA_START,
+		             DATA_END,
+		             page::EntryBits::ReadWrite.val(),
+		);
+		// Map bss section
+		id_map_range(
+		             &mut root,
+		             BSS_START,
+		             BSS_END,
+		             page::EntryBits::ReadWrite.val(),
+		);
+		// Map kernel stack
+		id_map_range(
+		             &mut root,
+		             KERNEL_STACK_START,
+		             KERNEL_STACK_END,
+		             page::EntryBits::ReadWrite.val(),
+		);
+	}
+page::map(
+	          &mut root,
+	          0x1000_0000,
+	          0x1000_0000,
+	          page::EntryBits::ReadWrite.val(),
+			  0
+	);
 
-
-
-
-
+	// CLINT
+	//  -> MSIP
+	page::map(
+	          &mut root,
+	          0x0200_0000,
+	          0x0200_0000,
+	          page::EntryBits::ReadWrite.val(),
+			  0
+	);
+	//  -> MTIMECMP
+	page::map(
+	          &mut root,
+	          0x0200_b000,
+	          0x0200_b000,
+	          page::EntryBits::ReadWrite.val(),
+			  0
+	);
+	//  -> MTIME
+	page::map(
+	          &mut root,
+	          0x0200_c000,
+	          0x0200_c000,
+	          page::EntryBits::ReadWrite.val(),
+			  0
+	);
+	// PLIC
+	id_map_range(
+	             &mut root,
+	             0x0c00_0000,
+	             0x0c00_2000,
+	             page::EntryBits::ReadWrite.val(),
+	);
+	id_map_range(
+	             &mut root,
+	             0x0c20_0000,
+	             0x0c20_8000,
+	             page::EntryBits::ReadWrite.val(),
+	);
 
 }
 
