acb2f7af9df1b443aca3289446e88c6477586c71
Tjoslef <josef.pasek17@gmail.com>
2024-09-01 23:14:11 +0200

    update 1.9

--- Os_kubrt/src/kmem.rs
+++ Os_kubrt/src/kmem.rs
@@ -12,7 +12,7 @@ struct AllocList {
 	pub flags_size: usize,
 }
 impl AllocList{
-    pub fn is taken(&self) -> bool{
+    pub fn is_taken(&self) -> bool{
     self.flags_size & AllocListFlags::Taken.val() !=0
     }
     pub fn is_free(&self) -> bool {
@@ -40,7 +40,36 @@ impl AllocList{
 	}
 }
 
-unsafe static mut KMEN_HEAD: *mut AllocList = null_mut();
-unsafe static mut KMEN_ALLOC: usize =0;
-unsafe static mut KMEN_PAGE_TABLE: *mut Table = null_mut();
+static mut KMEN_HEAD: *mut AllocList = null_mut();
+ static mut KMEN_ALLOC: usize =0;
+ static mut KMEN_PAGE_TABLE: *mut Table = null_mut();
+pub fn get_head()-> *mut u8{
+    unsafe{KMEN_HEAD as *mut u8}
 
+
+}
+pub fn get_page_table() -> *mut Table {
+	unsafe { KMEM_PAGE_TABLE as *mut Table }
+}
+
+pub fn get_num_allocations() -> usize {
+	unsafe { KMEM_ALLOC }
+}
+pub fn init() {
+unsafe {
+    let k_alloc = zalloc(64);
+    assert!(!k_alloc.is_null());
+    KMEN_ALLOC = 64;
+    KMEN_ALLOC = k_alloc as *mut AllocList;
+    (*KMEN_ALLOC).set_free();
+    (*KMEM_HEAD).set_size(KMEM_ALLOC * PAGE_SIZE);
+    KMEM_PAGE_TABLE = zalloc(1) as *mut Table;
+
+
+
+
+    }
+
+
+
+}
